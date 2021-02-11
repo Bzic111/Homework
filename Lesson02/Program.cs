7e98c76b37bcde87df73a555d84573c2c61e12cb
Иван Гаврилов <bzic1112@yandex.ru>
2021-02-11 23:23:29 +0300

    .vs восстановлена в .gitignore

---
+++ Lesson02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
