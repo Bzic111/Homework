f03a6d8f525ac7963dd4d1a3210cddd467dedfe6
Иван Гаврилов <bzic1112@yandex.ru>
2021-02-13 18:38:43 +0300

    ДЗ Вывод названия месяца.

--- Lesson02/Program.cs
+++ Lesson02/Program.cs
@@ -1,13 +1,42 @@
 ﻿using System;
 
-namespace Lesson02
+namespace Lesson2_1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
+            float minTemp;                                                              // минимальная температура
+            float maxTemp;                                                              // максимальная температура
+            float midTemp;                                                              // средняя температура
+            Console.Clear();                                                            // чистим консоль
+            Console.WriteLine("Введите минимальную температуру:");
+        GetMinTemp:                                                                     // точка возврата при ошибке ввода 1
+            if (float.TryParse(Console.ReadLine(), out float valMin)) 
+            {
+                minTemp = valMin;
+            }else
+            {
+                Console.WriteLine("Что-то пошло не так, попробуйте ещё раз.");
+                goto GetMinTemp;
+            } 
+
+
+            Console.WriteLine("Введите максимальную температуру:");
+        GetMaxTemp:                                                                     // точка возврата при ошибке ввода 2
+            if (float.TryParse(Console.ReadLine(), out float valMax))
+            {
+                maxTemp = valMax;
+            }
+            else
+            {
+                Console.WriteLine("Что-то пошло не так, попробуйте ещё раз.");
+                goto GetMaxTemp;
+            }
+            midTemp = (minTemp + maxTemp) / 2;                                           // вычисление средней температуры (a+b)/2
+            
+
+            Console.WriteLine($"Средняя температура: {Math.Round( midTemp,1)}");         // выводим округлённое значение
         }
     }
 }
