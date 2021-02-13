21708ef8db846b6ccbefe52899304ccffbb0f546
Иван Гаврилов <bzic1112@yandex.ru>
2021-02-13 19:12:10 +0300

    ДЗ Проверка числа на чётность

---
+++ Lesson2.3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lesson2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Введите число");
+            GetValue:
+            if (int.TryParse(Console.ReadLine(), out int val))
+            {
+                if (val % 2 == 0)
+                {
+                    Console.WriteLine($"Число чётное");
+                }
+                else
+                {
+                    Console.WriteLine($"Число не чётное");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Что-то пошло не так, попробуйте ещё раз.");
+                goto GetValue;
+            }
+        }
+    }
+}
