e24eaf4b223e064222d34e78e72e989a07ef96a2
Иван Гаврилов <bzic1112@yandex.ru>
2021-02-25 18:47:32 +0300

    ДЗ 4 начато

--- TestQuick1/Program.cs
+++ TestQuick1/Program.cs
@@ -2,76 +2,24 @@
 
 namespace TestQuick1
 {
-    class Program
+    public class Program
     {
+        
         static void Main(string[] args)
-        {                                                                               // Переменные
-            string[] arr;                                                               // Массив
-            string str;                                                                 // Строка для заполнения массива
-            string mover;                                                               // Число индекса сдвига
-            
-            Console.WriteLine("Введите значения для массива через пробел.");
-            str = Console.ReadLine();
-            Console.WriteLine("Введите число сдвига массива");
-            mover = Console.ReadLine();
-
-            if (Int32.TryParse(mover, out int move))
+        {
+            Console.WriteLine(Fibonachi(10));
+        }
+        static int Fibonachi(int n)
+        {
+            if (n>1)
             {
-                Console.WriteLine("move = " + move);
+                Console.WriteLine(n);
+                return (Fibonachi(n - 1) + Fibonachi(n - 2));
             }
             else
             {
-                move = 0;
-                Console.WriteLine("Error. move = " + move);
+                return n;
             }
-            str = str.Trim();                                                           // Очистка строки от пробелов в начале и в конце строки
-            arr = str.Split(" ", StringSplitOptions.RemoveEmptyEntries);                // Заполнение массива значениями
-            if (move < 0)                                                               // Цикл сдвига "влево"
-            {
-                string temp = arr[0];                                                   // временная переменная
-                do
-                {
-                    for (int i = 0; i < arr.Length; i++)
-                    {
-                        if (i < arr.Length - 1)
-                        {
-                            arr[i] = arr[i + 1];
-                        }
-                        else
-                        {
-                            arr[i] = temp;
-                        }
-                    }
-                } while (move != 0);
-            }
-            else if (move > 0)                                                          // Цикл сдвига "вправо"
-            {
-                string temp = arr[arr.Length - 1];                                      // временная переменная
-                do
-                {
-                    for (int i = arr.Length - 1; i >= 0; i--)
-                    {
-                        if (i > 0)
-                        {
-                            arr[i] = arr[i - 1];
-                        }
-                        else
-                        {
-                            arr[i] = temp;
-                        }
-                    }
-                    move--;
-                } while (move != 0);
-            }
-            else                                                                        // Сдвиг не производится
-            {
-                Console.WriteLine("move = " + move + " сдвиг не произведён.");
-            }
-
-            for (int i = 0; i < arr.Length; i++)                                        // Вывод цикла в консоль
-            {
-                Console.Write(arr[i] + " ");
-            }
-        }
+        }        
     }
 }
