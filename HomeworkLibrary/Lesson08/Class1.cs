dc7c03a29e65032d295bb3d23e7e86098e9d1e8e
Иван Гаврилов <bzic1112@yandex.ru>
2021-03-17 11:55:59 +0300

    Удаление ненужных каталогов, начало ДЗ 8

---
+++ HomeworkLibrary/Lesson08/Class1.cs
@@ -0,0 +1,28 @@
+﻿using MenuSpace;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson08
+{
+    public class HomeWork : MenuSpace.Work
+    {
+        public Menu.Runner[] AllRuns { get; }
+        string[] Names { get; }
+        public override string[] GetNames()
+        {
+            return Names;
+        }
+        public override Menu.Runner[] GetRunners()
+        {
+            return AllRuns;
+        }
+        public HomeWork()
+        {
+            AllRuns = new Menu.Runner[]
+            {
+
+            };
+        }
+    }
+}
